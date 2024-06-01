255343d2f25857eec9fcfbcce110717e95df9878
gonzalgu <gadol.gonzalez@gmail.com>
2024-06-01 16:26:06 -0400

    repl

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,22 +10,22 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	//fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Printf("error reading from input: %v", err)
+	for {
+		// Uncomment this block to pass the first stage
+		fmt.Fprint(os.Stdout, "$ ")
+		// Wait for user input
+		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Printf("error reading from input: %v", err)
+		}
+		cmd = cmd[:len(cmd)-1]
+		switch cmd {
+		case "echo":
+			fmt.Printf("echo received\n")
+		case "cd":
+			fmt.Printf("cd received\n")
+		default:
+			fmt.Printf("%s: command not found\n", cmd)
+		}
 	}
-	cmd = cmd[:len(cmd)-1]
-	switch cmd {
-	case "echo":
-		fmt.Printf("echo received\n")
-	case "cd":
-		fmt.Printf("cd received\n")
-	default:
-		fmt.Printf("%s: command not found\n", cmd)
-	}
-
 }
